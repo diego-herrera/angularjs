0eee9857308873df8dbc08bdfd675af5b0cb75ef
Diego Herrera <vermicida@gmail.com>
2014-11-20 15:15:42 +0100

    Ejemplo de eventos entre $scopes. Con $emit enviamos hacia arriba en la jerarquía de $scopes, y con $broadcast hacia abajo.

--- app/scripts/controllers/PelisCtrl.js
+++ app/scripts/controllers/PelisCtrl.js
@@ -3,4 +3,18 @@ angular
     .module("pelis")
     .controller("PelisCtrl", function($scope) {
 
+        // Con broadcast lanzamos eventos hacia abajo
+        // en la jerarquía de $scopes.
+        $scope.lanzarBroadcast = function() {
+
+          $scope.$broadcast("MiEvento", "Hacia abajo!!");
+
+        };
+
+        // Con $on nos suscribimos a eventos.
+        $scope.$on("MiOtroEvento", function(event, datos) {
+
+            alert(datos);
+
+        });
     });
--- app/scripts/controllers/PelisProxCtrl.js
+++ app/scripts/controllers/PelisProxCtrl.js
@@ -3,4 +3,18 @@ angular
     .module("pelis")
     .controller("PelisProxCtrl", function($scope) {
 
+        // Con $on nos suscribimos a eventos.
+        $scope.$on("MiEvento", function(event, datos) {
+
+            alert(datos);
+        });
+
+        // Con emit lanzamos eventos hacia arriba
+        // en la jerarquía de $scopes.
+        $scope.lanzarEmit = function() {
+
+            $scope.$emit("MiOtroEvento", "Hacia arriba!!");
+
+        };
+
     });
