8654cae14565becd57e99598d6c80dde7879d9fb
Diego Herrera <vermicida@gmail.com>
2014-11-21 12:24:46 +0100

    Hemos modificado la vista de detalle de película para mostrar su título y su imagen.

--- app/scripts/controllers/PeliDetalleCtrl.js
+++ app/scripts/controllers/PeliDetalleCtrl.js
@@ -1,8 +1,14 @@
 
 angular
     .module("pelis")
-    .controller("PeliDetalleCtrl", function($scope, Pelicula) {
+    .controller("PeliDetalleCtrl", function($scope, Pelicula, ApiService) {
 
+        $scope.pelicula = Pelicula.data;
 
+        // Esta función obtiene la ruta de la imagen.
+        $scope.obtenerRutaImagen = function(path) {
+
+            return ApiService.rutaImagen(path, 150);
+        };
 
     });
